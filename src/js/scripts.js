857705184b6a523b84038a9809614eef6401f61f
Yayuyn123 <yuy223@bu.edu>
2024-11-05 18:55:26 -0500

    Draft 1 Lets Jump

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,35 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+import './style.css';
+
+const cat = document.querySelector("#cat")
+const block = document.querySelector("#block")
+
+window.addEventListener("keydown", event => {
+    console.log(event);
+
+    if (event.code == "Space") {
+        console.log('Press Key Space');
+        cat.classList.add("jumpClass")
+
+        setTimeout(() => {
+            cat.classList.remove('jumpClass');
+        }, 300);
+    }
+});
+
+setInterval(() => {
+    const catBottom = parseFloat(
+        getComputedStyle(cat).getPropertyValue('bottom')
+    );
+    const blockLeft = parseFloat(
+        getComputedStyle(block).getPropertyValue('left')
+    );
+    if (blockLeft <20 && blockLeft > -20 && catBottom <= 20) {
+        console.log('Game Over');
+    }
+}, 10);
+
+
