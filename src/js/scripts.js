f96306979b0b967a5b5d6354f8378483257eb3f0
YellowStan <yellow@bu.edu>
2024-11-06 14:30:02 -0500

    Update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,41 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+// Stan
+
+const welcomePage = document.querySelector(".page1");
+
+console.log(welcomePage);
+
+function disappear1() {
+    welcomePage.classList.toggle("page2");
+}
+
+const choosePage = document.querySelector(".character-choose");
+
+function appear1() {
+    choosePage.classList.toggle("character-choose-change");
+}
+
+const animalPage = document.querySelector(".animals");
+
+function appear2() {
+    animalPage.classList.toggle("animals-change");
+}
+
+function disappear2() {
+    welcomePage.style.display = "none";
+}
+
+const toggleButton = document.querySelector(".page1");
+
+toggleButton.addEventListener("click", disappear1);
+
+toggleButton.addEventListener("click", appear1);
+
+toggleButton.addEventListener("click", appear2);
+
+toggleButton.addEventListener("click", disappear2);
+
+// /Stan
